dc322d5b0eb6884856f457fc1c488647c2140825
Vinícius Yoda <vyoda4604@gmail.com>
2024-12-14 10:51:02 -0300

    formAluno

--- src/main/java/br/com/academy/model/Aluno.java
+++ src/main/java/br/com/academy/model/Aluno.java
@@ -1,7 +1,14 @@
 package br.com.academy.model;
 
-
-import javax.persistence.*;
+import br.com.academy.Enum.Curso;
+import br.com.academy.Enum.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Aluno {
@@ -9,14 +16,21 @@ public class Aluno {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     @Column(name = "nome")
     private String nome;
+
     @Column(name = "curso")
+    @Enumerated(EnumType.STRING)
     private Curso curso;
+
     @Column(name = "matricula")
     private String matricula;
+
     @Column(name = "status")
+    @Enumerated(EnumType.STRING)
     private Status status;
+    
     @Column(name = "turno")
     private String turno;
 
