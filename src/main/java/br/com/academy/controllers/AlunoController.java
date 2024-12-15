f69df3714b0344aadce0a4d7044bb4ada030a3fa
Vinícius Yoda <vyoda4604@gmail.com>
2024-12-15 13:38:49 -0300

    alterar e deletar

--- src/main/java/br/com/academy/controllers/AlunoController.java
+++ src/main/java/br/com/academy/controllers/AlunoController.java
@@ -3,6 +3,7 @@ package br.com.academy.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,8 +27,39 @@ public class AlunoController {
     @PostMapping("InsertAlunos")
     public ModelAndView inserirAlunos(Aluno aluno){
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("redirect:/Aluno/listAluno");
+        mv.setViewName("redirect:/alunos-adicionados");
         alunorepositorio.save(aluno);
         return mv;
+    }
+
+    @GetMapping("alunos-adicionados")
+    public ModelAndView listagemAlunos(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("Aluno/listAluno");
+        mv.addObject("alunosList", alunorepositorio.findAll());
+        return mv;
+    }
+
+    @GetMapping("/alterar/{id}")
+    public ModelAndView alterar(@PathVariable("id") Integer id) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("Aluno/alterar");
+        Aluno aluno = alunorepositorio.getOne(id);
+        mv.addObject("aluno", aluno);
+        return mv;
+    }
+
+    @PostMapping("/alterar")
+    public ModelAndView alterar(Aluno aluno){
+        ModelAndView mv = new ModelAndView();
+        alunorepositorio.save(aluno);
+        mv.setViewName("redirect:/alunos-adicionados");
+        return mv;
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluirAluno(@PathVariable("id") Integer id){
+        alunorepositorio.deleteById(id);
+        return "redirect:/alunos-adicionados";
     }
 }
