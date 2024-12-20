9a842f2aa7c4ae3fa63d50e922a111346343b136
Vinícius Yoda <vyoda4604@gmail.com>
2024-12-20 10:28:53 -0300

    util

---
+++ src/main/java/br/com/academy/service/ServiceUsuario.java
@@ -0,0 +1,18 @@
+package br.com.academy.service;
+
+import org.springframework.stereotype.Service;
+
+import br.com.academy.moedl.Usuario;
+
+@Service
+public class ServiceUsuario {
+]
+    @Autowired
+    private UsuarioDao repositorioUsuario;
+
+    public void salvarUsuario(Usuario user) throws Exception {
+        try {
+            if (repositorioUsuario.)
+        }
+    }
+}
